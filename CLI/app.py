41fd21c4febb39478635a371a7f5a8594e7e8da1
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-12-07 04:42:41 +0530

    Completed Gaussian Blur (Including Tests)

--- CLI/app.py
+++ CLI/app.py
@@ -1,5 +1,44 @@
-from .utils import READ_PATH, SAVE_PATH, Processor
+import os
+import sys
+
+from .utils import READ_PATH, Processor, read_image, save_image, show
 
 
 def run():
-    pass
+    args_1 = ["--file", "-f"]
+    args_2 = ["--gauss-blur", "-gb"]
+    args_10 = ["--save", "-s"]
+
+    filename = None
+    do_gauss_blur = False
+    save = False
+
+    if args_1[0] in sys.argv: filename = sys.argv[sys.argv.index(args_1[0]) + 1]
+    if args_1[1] in sys.argv: filename = sys.argv[sys.argv.index(args_1[1]) + 1]
+
+    if args_2[0] in sys.argv: 
+        do_gauss_blur = True
+        setup = sys.argv[sys.argv.index(args_2[0]) + 1] + ","
+        gaussian_blur_kernel_size = setup.split(",")[0]
+        gaussian_blur_sigmaX = setup.split(",")[1]
+    if args_2[1] in sys.argv: 
+        do_gauss_blur = True
+        setup = sys.argv[sys.argv.index(args_2[1]) + 1] + ","
+        gaussian_blur_kernel_size = setup.split(",")[0]
+        gaussian_blur_sigmaX = setup.split(",")[1]
+    
+    if args_10[0] in sys.argv: save = True
+    if args_10[1] in sys.argv: save = True
+
+    assert filename is not None, "Missing value for --file | -f"
+    assert filename in os.listdir(READ_PATH), "File Not Found"
+
+    image = read_image(os.path.join(READ_PATH, filename))
+
+    if do_gauss_blur:
+        image = Processor.gauss_blur(image=image, kernel_size=gaussian_blur_kernel_size, sigmaX=gaussian_blur_sigmaX)
+    
+    if not save:
+        show(image)
+    else:
+        save_image(image)
--- CLI/utils.py
+++ CLI/utils.py
@@ -1,4 +1,7 @@
 import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 READ_PATH = "Files"
@@ -9,9 +12,42 @@ if not os.path.exists(SAVE_PATH):
     os.makedirs(SAVE_PATH)
 
 
+def read_image(path: str) -> np.ndarray:
+    return cv2.imread(path, cv2.IMREAD_COLOR)
+
+
+def save_image(image) -> None:
+    cv2.imwrite(os.path.join(SAVE_PATH, "Processed.jpg"), image)
+
+
+def show(image: np.ndarray, title=None):
+    plt.figure()
+    plt.imshow(cv2.cvtColor(src=image, code=cv2.COLOR_BGR2RGB))
+    plt.axis("off")
+    if title:
+        plt.title(title)
+    figmanager = plt.get_current_fig_manager()
+    figmanager.window.state("zoomed")
+    plt.show()
+
+
 class ImageProcessor(object):
     def __init__(self):
         pass
+
+    def gauss_blur(self, image: np.ndarray, kernel_size: int, sigmaX: float):
+        kernel_size = int(kernel_size)
+
+        if kernel_size == 1:
+            kernel_size = 3
+        
+        if kernel_size % 2 == 0:
+            kernel_size += 1
+        
+        if sigmaX != "": sigmaX = int(sigmaX)
+        else: sigmaX = 0
+
+        return cv2.GaussianBlur(src=image, ksize=(kernel_size, kernel_size), sigmaX=sigmaX)
 
 
 Processor = ImageProcessor()
