f6773b31eb3e497766a627c33b2e9ead37aa885f
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-12-07 09:20:53 +0530

    Complete

--- CLI/app.py
+++ CLI/app.py
@@ -13,9 +13,14 @@ def run():
     args_6  = ["--linear", "-l"]
     args_7  = ["--clahe", "-ae"]
     args_8  = ["--hist-equ", "-he"]
-    args_9  = ["--hue", "-h"]
+    args_9  = ["--hue"]
     args_10 = ["--saturation", "-sat"]
     args_11 = ["--vibrance", "-v"]
+    args_12 = ["--width", "-w"]
+    args_13 = ["--height", "-h"]
+    args_14 = ["--sharpen", "-sh"]
+    args_15 = ["--posterize", "-post"]
+    args_16 = ["--dither", "-dit"]
     args_20 = ["--save", "-s"]
 
     filename = None
@@ -29,6 +34,10 @@ def run():
     do_hue = False
     do_saturation = False
     do_vibrance = False
+    width, height = None, None
+    do_sharpen = False
+    do_posterize = False
+    do_dither = False
     save = False
 
     if args_1[0] in sys.argv: filename = sys.argv[sys.argv.index(args_1[0]) + 1]
@@ -92,9 +101,6 @@ def run():
     if args_9[0] in sys.argv:
         do_hue = True
         hue = float(sys.argv[sys.argv.index(args_9[0]) + 1])
-    if args_9[1] in sys.argv:
-        do_hue = True
-        hue = float(sys.argv[sys.argv.index(args_9[1]) + 1])
     
     if args_10[0] in sys.argv:
         do_saturation = True
@@ -109,6 +115,38 @@ def run():
     if args_11[1] in sys.argv:
         do_vibrance = True
         vibrance = float(sys.argv[sys.argv.index(args_11[1]) + 1])
+    
+    if args_12[0] in sys.argv:
+        width = int(sys.argv[sys.argv.index(args_12[0]) + 1])
+    if args_12[1] in sys.argv:
+        width = int(sys.argv[sys.argv.index(args_12[1]) + 1])
+    
+    if args_13[0] in sys.argv:
+        height = int(sys.argv[sys.argv.index(args_13[0]) + 1])
+    if args_13[1] in sys.argv:
+        height = int(sys.argv[sys.argv.index(args_13[1]) + 1])
+    
+    if args_14[0] in sys.argv:
+        do_sharpen = True
+        sharpen_kernel_size = sys.argv[sys.argv.index(args_14[0]) + 1]
+    if args_14[1] in sys.argv:
+        do_sharpen = True
+        sharpen_kernel_size = sys.argv[sys.argv.index(args_14[1]) + 1]
+    
+    if args_15[0] in sys.argv:
+        do_dither = True
+        num_colors = int(sys.argv[sys.argv.index(args_15[0]) + 1])
+    if args_16[1] in sys.argv:
+        do_dither = True
+        num_colors = int(sys.argv[sys.argv.index(args_16[1]) + 1])
+    
+    if args_16[0] in sys.argv:
+        do_posterize = True
+        num_colors = int(sys.argv[sys.argv.index(args_16[0]) + 1])
+    if args_15[1] in sys.argv:
+        do_posterize = True
+        num_colors = int(sys.argv[sys.argv.index(args_15[1]) + 1])
+
   
     if args_20[0] in sys.argv: save = True
     if args_20[1] in sys.argv: save = True
@@ -128,7 +166,21 @@ def run():
     if do_hue: image = Processor.adjust_hue(image=image, hue=hue)
     if do_saturation: image = Processor.adjust_saturation(image=image, saturation=saturation)
     if do_vibrance: image = Processor.adjust_vibrance(image=image, vibrance=vibrance)
+    if do_sharpen: image = Processor.sharpen(image=image, kernel_size=sharpen_kernel_size)
+    if do_posterize: image = Processor.posterize_image(image=image, num_colors=num_colors)
+    if do_dither: image = Processor.dither_image(image=image, num_colors=num_colors)
 
+
+    if isinstance(width, int) and height is None:
+        h, _, _ = image.shape
+        image = Processor.resize_image(image, width, h)
+
+    if width is None and isinstance(height, int):
+        _, w, _ = image.shape
+        image = Processor.resize_image(image, w, height)
+    
+    if isinstance(width, int) and isinstance(height, int):
+        image = Processor.resize_image(image, width, height)
 
     if not save: show(image)
     else: save_image(image)
--- CLI/utils.py
+++ CLI/utils.py
@@ -11,6 +11,7 @@ SAVE_PATH = "Processed"
 if not os.path.exists(SAVE_PATH):
     os.makedirs(SAVE_PATH)
 
+#######################################################################################################
 
 def read_image(path: str) -> np.ndarray:
     return cv2.imread(path, cv2.IMREAD_COLOR)
@@ -30,10 +31,28 @@ def show(image: np.ndarray, title=None):
     figmanager.window.state("zoomed")
     plt.show()
 
+#######################################################################################################
+
+def new_color(pixel: int, num_colors: int) -> int:
+    colors = [(1/num_colors)*i for i in range(num_colors)]
+    distances = [abs(pixel-colors[i]) for i in range(len(colors))]
+    index = distances.index(min(distances))
+    return colors[index]
+
+
+def find_closest_color(pixel, num_colors):
+    colors = [i*(1/num_colors) for i in range(num_colors+1)]
+    distances = [abs(colors[i]-pixel) for i in range(len(colors))]
+    index = distances.index(min(distances))
+    return colors[index]
+
+#######################################################################################################
 
 class ImageProcessor(object):
     def __init__(self):
         pass
+
+#######################################################################################################
 
     def gauss_blur(self, image: np.ndarray, kernel_size, sigmaX) -> np.ndarray:
         kernel_size = int(kernel_size)
@@ -62,7 +81,9 @@ class ImageProcessor(object):
             kernel_size += 1
 
         return cv2.medianBlur(src=image, ksize=kernel_size)
-    
+
+#######################################################################################################
+
     def adjust_gamma(self, image: np.ndarray, gamma: float) -> np.ndarray:
         image = image / 255
         image = np.clip(((image ** gamma) * 255), 0, 255).astype("uint8")
@@ -90,26 +111,82 @@ class ImageProcessor(object):
         else:
             image = cv2.equalizeHist(image)
         return image
-    
-    def adjust_hue(self, image: np.ndarray, hue: float):
+
+#######################################################################################################
+
+    def adjust_hue(self, image: np.ndarray, hue: float) -> np.ndarray:
         image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2HSV)
         feature = image[:, :, 0]
         feature = np.clip((hue * feature), 0, 179).astype("uint8")
         image[:, :, 0] = feature
         return cv2.cvtColor(src=image, code=cv2.COLOR_HSV2BGR)
 
-    def adjust_saturation(self, image: np.ndarray, saturation: float):
+    def adjust_saturation(self, image: np.ndarray, saturation: float) -> np.ndarray:
         image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2HSV)
         feature = image[:, :, 1]
         feature = np.clip((saturation * feature), 0, 255).astype("uint8")
         image[:, :, 1] = feature
         return cv2.cvtColor(src=image, code=cv2.COLOR_HSV2BGR)
 
-    def adjust_vibrance(self, image: np.ndarray, vibrance: float):
+    def adjust_vibrance(self, image: np.ndarray, vibrance: float) -> np.ndarray:
         image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2HSV)
         feature = image[:, :, 2]
         feature = np.clip((vibrance * feature), 0, 255).astype("uint8")
         image[:, :, 2] = feature
         return cv2.cvtColor(src=image, code=cv2.COLOR_HSV2BGR)
 
+#######################################################################################################
+
+    def resize_image(self, image: np.ndarray, width: int, height: int) -> np.ndarray:
+        return cv2.resize(src=image, dsize=(width, height), interpolation=cv2.INTER_AREA)
+    
+#######################################################################################################
+
+    def sharpen(self, image: np.ndarray, kernel_size):
+        kernel_size = int(kernel_size)
+
+        if kernel_size % 2 == 0:
+            kernel_size += 1
+
+        kernel = cv2.getStructuringElement(shape=cv2.MORPH_CROSS, ksize=(kernel_size, kernel_size)) * -1
+        kernel[int(kernel_size / 2), int(kernel_size / 2)] = ((kernel_size - 1) * 2) + 1
+
+        image = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+        image = np.clip(image, 0, 255).astype("uint8")
+        return image
+
+#######################################################################################################
+    
+    def posterize_image(self, image: np.ndarray, num_colors: int) -> np.ndarray:
+        h, w, c = image.shape
+        image = image / 255
+        for c in range(c):
+            for i in range(h):
+                for j in range(w):
+                    image[i][j][c] = new_color(image[i][j][c], num_colors)
+        return np.clip((image*255), 0, 255).astype("uint8")
+
+#######################################################################################################
+    
+    def dither_image(self, image: np.ndarray, num_colors: int) -> np.ndarray:
+        image = image / 255
+        h, w, c = image.shape
+        for c in range(c):
+            for i in range(h-1):
+                for j in range(1, w-1):
+                    old_pixel = image[i][j][c]
+                    new_pixel = find_closest_color(old_pixel, num_colors)
+                    image[i][j][c] = new_pixel
+                    quant_error = old_pixel - new_pixel
+
+                    image[i][j+1][c]   = image[i][j+1][c] + (quant_error * 7/16)
+                    image[i+1][j+1][c] = image[i+1][j+1][c] + (quant_error * 1/16)
+                    image[i+1][j][c]   = image[i+1][j][c] + (quant_error * 5/16)
+                    image[i+1][j-1][c] = image[i+1][j-1][c] + (quant_error * 3/16)
+        return np.clip((image*255), 0, 255).astype("uint8")
+
+#######################################################################################################
+
 Processor = ImageProcessor()
+
+#######################################################################################################
